ce805ffb55b47f49cb739928ec9c9eea5cb2ed77
EugeniaO <eugenia.ovcharenko@gmail.com>
2023-08-18 13:54:12 +0300

    added second task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+
+const markup = galleryItems.map(({preview, description, original}) => `<li class="gallery__item"> <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+<img src="${preview}" alt="${description}" class="gallery__image" data-source="${original}"/>  
+  </a>
+</li>`).join("");
+
+
+listEl.insertAdjacentHTML("beforeend", markup);
+
+
+listEl.addEventListener("click", onClick);
+
+function onClick  (event) {
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    const imageId = event.target.dataset.source;
+    const currentItem = galleryItems.find(({ original }) => original === imageId);
+    console.log(currentItem);
+    const instance = basicLightbox.create(`<div class="modal">
+        <img src="${currentItem.original}" alt="${currentItem.description}"  class="gallery__image" />
+    </div>`);
+ instance.show();
+
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+
+const markup = galleryItems.map(({preview, description, original}) => `<li class="gallery__item"> <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+<img src="${preview}" alt="${description}" class="gallery__image"/>  
+  </a>
+</li>`).join("");
+
+
+listEl.insertAdjacentHTML("beforeend", markup);
+
+// let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// Do something…
+// });
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: '250', captionsData: 'alt' });
+
+
+
+
